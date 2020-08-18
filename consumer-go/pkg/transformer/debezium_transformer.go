dd5915ed96b1a5ff92060eb964bd1a186062d137
Alok Kumar Singh <mail.alok87@gmail.com>
2020-08-18 21:58:44 +0530

    Format code

--- consumer-go/pkg/transformer/debezium_transformer.go
+++ consumer-go/pkg/transformer/debezium_transformer.go
@@ -1,59 +1,59 @@
 package transformer
 
 import (
-    "fmt"
+	"fmt"
 )
 
-type debeziumTransformer struct {}
+type debeziumTransformer struct{}
 
 func (d *debeziumTransformer) extract(key string, dataI map[string]interface{},
-    result map[string]string) map[string]string {
+	result map[string]string) map[string]string {
 
-    data := dataI[key].(map[string]interface{})
-    if data == nil {
-        return result
-    }
+	data := dataI[key].(map[string]interface{})
+	if data == nil {
+		return result
+	}
 
-    for _, v := range data {
-        for k2, v2 := range v.(map[string]interface{}) {
-            switch v2.(type) {
-            case map[string]interface{}:
-                for _, v3 := range v2.(map[string]interface{}) {
-                    result[k2] = fmt.Sprintf("%v", v3)
-                }
-            default:
-                result[k2] = fmt.Sprintf("%v", v2)
-            }
-        }
-    }
+	for _, v := range data {
+		for k2, v2 := range v.(map[string]interface{}) {
+			switch v2.(type) {
+			case map[string]interface{}:
+				for _, v3 := range v2.(map[string]interface{}) {
+					result[k2] = fmt.Sprintf("%v", v3)
+				}
+			default:
+				result[k2] = fmt.Sprintf("%v", v2)
+			}
+		}
+	}
 
-    return result
+	return result
 }
 
 func (d *debeziumTransformer) after(native interface{}) map[string]string {
-        result := make(map[string]string)
-        if native == nil {
-            return result
-        }
+	result := make(map[string]string)
+	if native == nil {
+		return result
+	}
 
-        data := native.(map[string]interface{})
-        if data == nil {
-            return result
-        }
+	data := native.(map[string]interface{})
+	if data == nil {
+		return result
+	}
 
-        return d.extract("after", data, result)
+	return d.extract("after", data, result)
 }
 
 func (d *debeziumTransformer) before(native interface{}) map[string]string {
-        result := make(map[string]string)
-        if native == nil {
-            return result
-        }
+	result := make(map[string]string)
+	if native == nil {
+		return result
+	}
 
-        data := native.(map[string]interface{})
-        if data == nil {
-            return result
-        }
+	data := native.(map[string]interface{})
+	if data == nil {
+		return result
+	}
 
-        return d.extract("before", data, result)
+	return d.extract("before", data, result)
 }
