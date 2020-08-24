2c29c34d78a202f184234d9b25396eccf1b329f9
Alok Kumar Singh <mail.alok87@gmail.com>
2020-08-24 19:38:06 +0530

    Redshift library and transformations

--- kafka-go/pkg/transformer/transformer.go
+++ kafka-go/pkg/transformer/transformer.go
@@ -8,59 +8,10 @@ import (
 )
 
 type Transformer interface {
-	Transform(message *serializer.Message) error
+	TransformSchema(schema string) (interface{}, error)
+	TransformMessage(message *serializer.Message) error
 }
 
 func NewTransformer() Transformer {
 	return &redshiftTransformer{}
-}
-
-type redshiftTransformer struct{}
-
-func (c *redshiftTransformer) getOperation(
-	message *serializer.Message,
-	before map[string]string,
-	after map[string]string) (string, error) {
-
-	r := 0
-	if len(before) != 0 {
-		r += 1
-	}
-	if len(after) != 0 {
-		r += 2
-	}
-	switch r {
-	case 0:
-		return "", fmt.Errorf(
-			"message: %v has both before and after as nil\n", message)
-	case 1:
-		return "DELETE", nil
-	case 2:
-		return "CREATE", nil
-	case 3:
-		return "UPDATE", nil
-	default:
-		return "", fmt.Errorf(
-			"message: %v not possible get operation\n", message)
-	}
-}
-
-func (c *redshiftTransformer) Transform(message *serializer.Message) error {
-	d := &debeziumParser{}
-
-	before := d.before(message.Value)
-	after := d.after(message.Value)
-
-	operation, err := c.getOperation(message, before, after)
-	if err != nil {
-		return err
-	}
-	after["operation"] = operation
-
-	message.Value, err = json.Marshal(after)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
