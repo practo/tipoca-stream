e98541c915c330fc040e2c30aae3b401477ab278
Alok Kumar Singh <mail.alok87@gmail.com>
2020-08-27 12:59:24 +0530

    Fix the database queries

--- kafka-go/pkg/redshift/redshift.go
+++ kafka-go/pkg/redshift/redshift.go
@@ -15,6 +15,34 @@ import (
 	"strings"
 )
 
+const (
+	schemaExist  = `select schema_name from information_schema.schemata where schema_name='%s';`
+	schemaCreate = `create schema "%s";`
+	tableExist   = `select table_name from information_schema.tables where table_schema="%s" and table_name="%s";`
+	tableCreate  = `CREATE TABLE "%s"."%s" (%s);`
+	// returns one row per column with the attributes:
+	// name, type, default_val, not_null, primary_key, dist_key, and sort_ordinal
+	// need to pass a schema and table name as the parameters
+	tableSchema = `SELECT
+  f.attname AS name,
+  pg_catalog.format_type(f.atttypid,f.atttypmod) AS col_type,
+  CASE
+      WHEN f.atthasdef = 't' THEN d.adsrc
+      ELSE ''
+  END AS default_val,
+  f.attnotnull AS not_null,
+  p.contype IS NOT NULL AND p.contype = 'p' AS primary_key
+FROM pg_attribute f
+  JOIN pg_class c ON c.oid = f.attrelid
+  LEFT JOIN pg_attrdef d ON d.adrelid = c.oid AND d.adnum = f.attnum
+  LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+  LEFT JOIN pg_constraint p ON p.conrelid = c.oid AND f.attnum = ANY (p.conkey)
+WHERE c.relkind = 'r'::char
+    AND n.nspname = '%s'  -- Replace with schema name
+    AND c.relname = '%s'  -- Replace with table name
+     AND f.attnum > 0 ORDER BY f.attnum;`
+)
+
 type dbExecCloser interface {
 	Close() error
 	BeginTx(c context.Context, opts *sql.TxOptions) (*sql.Tx, error)
@@ -86,34 +114,6 @@ type ColInfo struct {
 	PrimaryKey bool   `json:"primarykey"`
 }
 
-const (
-	schemaExist  = `SELECT schema_name FROM information_schema.schemata WHERE schema_name="%s";`
-	schemaCreate = `CREATE SCHEMA "%s";`
-	tableExist   = `SELECT table_name FROM information_schema.tables WHERE table_schema="%s" AND table_name="%s";`
-	tableCreate  = `CREATE TABLE "%s"."%s" (%s);`
-	// returns one row per column with the attributes:
-	// name, type, default_val, not_null, primary_key, dist_key, and sort_ordinal
-	// need to pass a schema and table name as the parameters
-	tableSchema = `SELECT
-  f.attname AS name,
-  pg_catalog.format_type(f.atttypid,f.atttypmod) AS col_type,
-  CASE
-      WHEN f.atthasdef = 't' THEN d.adsrc
-      ELSE ''
-  END AS default_val,
-  f.attnotnull AS not_null,
-  p.contype IS NOT NULL AND p.contype = 'p' AS primary_key
-FROM pg_attribute f
-  JOIN pg_class c ON c.oid = f.attrelid
-  LEFT JOIN pg_attrdef d ON d.adrelid = c.oid AND d.adnum = f.attnum
-  LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
-  LEFT JOIN pg_constraint p ON p.conrelid = c.oid AND f.attnum = ANY (p.conkey)
-WHERE c.relkind = 'r'::char
-    AND n.nspname = '%s'  -- Replace with schema name
-    AND c.relname = '%s'  -- Replace with table name
-     AND f.attnum > 0 ORDER BY f.attnum;`
-)
-
 func (r *Redshift) SchemaExist(schema string) (bool, error) {
 	q := fmt.Sprintf(schemaExist, schema)
 	var placeholder string
@@ -125,9 +125,7 @@ func (r *Redshift) SchemaExist(schema string) (bool, error) {
 			)
 			return false, nil
 		}
-		// TODO: fix me
-		// return false, fmt.Errorf("error querying schema exist: %v\n", err)
-		return false, nil
+		return false, fmt.Errorf("error querying schema exist: %v\n", err)
 	}
 
 	return true, nil
@@ -135,7 +133,6 @@ func (r *Redshift) SchemaExist(schema string) (bool, error) {
 
 func (r *Redshift) CreateSchema(tx *sql.Tx, schema string) error {
 	createSQL := fmt.Sprintf(schemaCreate, schema)
-	klog.V(5).Infof("Creating Schema: sql=%s\n", createSQL)
 	createStmt, err := tx.PrepareContext(r.ctx, createSQL)
 	if err != nil {
 		return err
@@ -152,7 +149,7 @@ func (r *Redshift) TableExist(schema string, table string) (bool, error) {
 	if err != nil {
 		if err == sql.ErrNoRows {
 			klog.V(5).Infof(
-				"schema: %s, table: %s does not exist", schema, table,
+				"schema:%s, table:%s: does not exist", schema, table,
 			)
 			return false, nil
 		}
@@ -185,7 +182,7 @@ func getColumnSQL(c ColInfo) string {
 	// currently we don't support that
 	defaultVal := ""
 	if c.DefaultVal != "" {
-		defaultVal = fmt.Sprintf("DEFAULT %s", c.DefaultVal)
+		defaultVal = fmt.Sprintf("DEFAULT '%s'", c.DefaultVal)
 	}
 	notNull := ""
 	if c.NotNull {
@@ -216,17 +213,15 @@ func (r *Redshift) CreateTable(tx *sql.Tx, table Table) error {
 	createSQL := fmt.Sprintf(
 		tableCreate,
 		table.Meta.Schema,
-		table,
+		table.Name,
 		strings.Join(columnSQL, ","),
 	)
-	klog.V(5).Infof("Creating Table: sql=%s\n", createSQL)
 
 	createStmt, err := tx.PrepareContext(r.ctx, createSQL)
 	if err != nil {
 		return fmt.Errorf("error preparing statement: %v\n", err)
 	}
 
-	klog.V(5).Infof("Running command: %s with args: %v\n", createSQL, args)
 	_, err = createStmt.ExecContext(r.ctx)
 
 	return err
