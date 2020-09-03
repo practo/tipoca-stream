04b146832b5574b2bd6bf1de497721c036c58407
Alok Kumar Singh <mail.alok87@gmail.com>
2020-09-03 20:11:38 +0530

    Redshift requires lower case

--- kafka-go/build/offset_table_example.sql
+++ kafka-go/build/offset_table_example.sql
@@ -1,7 +1,7 @@
 CREATE TABLE currentTable
-( kafkaOffset INT(11),
+( kafkaoffset INT(11),
 operation VARCHAR(32) NOT NULL,
-otherTablePK INT(11) NOT NULL,
-otherTableOrderStatus VARCHAR(32) NOT NULL,
-CONSTRAINT key1 PRIMARY KEY (kafkaOffset)
+othertablePK INT(11) NOT NULL,
+othertableorderstatus VARCHAR(32) NOT NULL,
+CONSTRAINT key1 PRIMARY KEY (kafkaoffset)
 );
