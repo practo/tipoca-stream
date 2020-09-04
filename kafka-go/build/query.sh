6d98299e327317a5c5961e01d7d6759c90b9bbe7
Alok Kumar Singh <alok.singh@practo.com>
2020-09-04 07:54:26 +0000

    Changes for test mysql

--- kafka-go/build/query.sh
+++ kafka-go/build/query.sh
@@ -2,4 +2,4 @@
 
 pod=`kubectl get pods -n kafka | grep inventory-mysql  | awk '{print $1}'`
 
-kubectl exec -it -n kafka $pod bash /var/lib/mysql/lmysql
+kubectl exec -it -n kafka $pod bash /usr/local/bin/lmysql
