4ca34144171b97d3401f01296847e5bde464c63d
Alok Kumar Singh <mail.alok87@gmail.com>
2020-09-08 17:23:46 +0530

    Use pointers to support null values

--- kafka-go/pkg/transformer/debezium_msg_transformer.go
+++ kafka-go/pkg/transformer/debezium_msg_transformer.go
@@ -19,13 +19,12 @@ type debeziumMsgParser struct {
 	msg interface{}
 }
 
-func (d *debeziumMsgParser) extract(
-	key string, msg map[string]interface{},
-	result map[string]string) map[string]string {
+func (d *debeziumMsgParser) extract(key string, msg map[string]interface{},
+	result map[string]*string) {
 
 	dataKey := msg[key]
 	if dataKey == nil {
-		return result
+		return
 	}
 
 	data := dataKey.(map[string]interface{})
@@ -36,19 +35,37 @@ func (d *debeziumMsgParser) extract(
 			switch v2.(type) {
 			case map[string]interface{}:
 				for _, v3 := range v2.(map[string]interface{}) {
-					result[strings.ToLower(k2)] = fmt.Sprintf("%v", v3)
+					columnValue := fmt.Sprintf("%v", v3)
+					result[strings.ToLower(k2)] = &columnValue
 				}
+			case nil:
+				result[strings.ToLower(k2)] = nil
 			default:
-				result[strings.ToLower(k2)] = fmt.Sprintf("%v", v2)
+				columnValue := fmt.Sprintf("%v", v2)
+				result[strings.ToLower(k2)] = &columnValue
 			}
 		}
 	}
+}
+
+func (d *debeziumMsgParser) after() map[string]*string {
+	result := make(map[string]*string)
+	if d.msg == nil {
+		return result
+	}
+
+	data := d.msg.(map[string]interface{})
+	if data == nil {
+		return result
+	}
+
+	d.extract("after", data, result)
 
 	return result
 }
 
-func (d *debeziumMsgParser) after() map[string]string {
-	result := make(map[string]string)
+func (d *debeziumMsgParser) before() map[string]*string {
+	result := make(map[string]*string)
 	if d.msg == nil {
 		return result
 	}
@@ -58,21 +75,9 @@ func (d *debeziumMsgParser) after() map[string]string {
 		return result
 	}
 
-	return d.extract("after", data, result)
-}
+	d.extract("before", data, result)
 
-func (d *debeziumMsgParser) before() map[string]string {
-	result := make(map[string]string)
-	if d.msg == nil {
-		return result
-	}
-
-	data := d.msg.(map[string]interface{})
-	if data == nil {
-		return result
-	}
-
-	return d.extract("before", data, result)
+	return result
 }
 
 func NewMsgTransformer() MsgTransformer {
@@ -81,16 +86,14 @@ func NewMsgTransformer() MsgTransformer {
 
 type debeziumMsgTransformer struct{}
 
-func (c *debeziumMsgTransformer) getOperation(
-	message *serializer.Message,
-	before map[string]string,
-	after map[string]string) (string, error) {
+func (c *debeziumMsgTransformer) getOperation(message *serializer.Message,
+	beforeLen int, afterLen int) (string, error) {
 
 	r := 0
-	if len(before) != 0 {
+	if beforeLen != 0 {
 		r += 1
 	}
-	if len(after) != 0 {
+	if afterLen != 0 {
 		r += 2
 	}
 	switch r {
@@ -120,13 +123,15 @@ func (c *debeziumMsgTransformer) Transform(
 	before := d.before()
 	after := d.after()
 
-	operation, err := c.getOperation(message, before, after)
+	operation, err := c.getOperation(message, len(before), len(after))
 	if err != nil {
 		return err
 	}
+
 	// redshift only has all columns as lower cases
-	after["kafkaoffset"] = fmt.Sprintf("%v", message.Offset)
-	after["operation"] = operation
+	kafkaOffset := fmt.Sprintf("%v", message.Offset)
+	after["kafkaoffset"] = &kafkaOffset
+	after["operation"] = &operation
 
 	message.Value, err = json.Marshal(after)
 	if err != nil {
