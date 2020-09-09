f2584fd9783c256717a6cb8aefb224354370d6c6
Alok Kumar Singh <alok.singh@practo.com>
2020-09-09 08:03:35 +0000

    Creater masker only when it is enabled and minor additons

--- kafka-go/pkg/transformer/debezium_schema_transformer_test.go
+++ kafka-go/pkg/transformer/debezium_schema_transformer_test.go
@@ -1,6 +1,7 @@
 package transformer
 
 import (
+	"github.com/practo/tipoca-stream/kafka-go/pkg/redshift"
 	"testing"
 )
 
@@ -37,7 +38,17 @@ func TestSchemaTransformMapWithNulls(t *testing.T) {
 	jobSchema := `{"type":"record","name":"Envelope","namespace":"datapipe.inventory.customers","fields":[{"name":"before","type":["null",{"type":"record","name":"Value","fields":[{"name":"id","type":"int"},{"name":"first_name","type":"string"},{"name":"last_name","type":["null","string"],"default":null},{"name":"email","type":"string"}],"connect.name":"datapipe.inventory.customers.Value"}],"default":null},{"name":"after","type":["null","Value"],"default":null},{"name":"source","type":{"type":"record","name":"Source","namespace":"io.debezium.connector.mysql","fields":[{"name":"version","type":"string"},{"name":"connector","type":"string"},{"name":"name","type":"string"},{"name":"ts_ms","type":"long"},{"name":"snapshot","type":[{"type":"string","connect.version":1,"connect.parameters":{"allowed":"true,last,false"},"connect.default":"false","connect.name":"io.debezium.data.Enum"},"null"],"default":"false"},{"name":"db","type":"string"},{"name":"table","type":["null","string"],"default":null},{"name":"server_id","type":"long"},{"name":"gtid","type":["null","string"],"default":null},{"name":"file","type":"string"},{"name":"pos","type":"long"},{"name":"row","type":"int"},{"name":"thread","type":["null","long"],"default":null},{"name":"query","type":["null","string"],"default":null}],"connect.name":"io.debezium.connector.mysql.Source"}},{"name":"op","type":"string"},{"name":"ts_ms","type":["null","long"],"default":null},{"name":"transaction","type":["null",{"type":"record","name":"ConnectDefault","namespace":"io.confluent.connect.avro","fields":[{"name":"id","type":"string"},{"name":"total_order","type":"long"},{"name":"data_collection_order","type":"long"}]}],"default":null}],"connect.name":"datapipe.inventory.customers.Envelope"}`
 
 	c := &debeziumSchemaTransformer{srclient: nil}
-	_, err := c.transformSchemaValue(jobSchema, "id", "int")
+	r, err := c.transformSchemaValue(jobSchema, "id", "int")
+
+	table := r.(redshift.Table)
+	nullColumn := table.Columns[2]
+	if nullColumn.NotNull == false {
+		t.Errorf("Expected NotNull: true, got: %+v\n", nullColumn)
+	}
+	if nullColumn.Type != "string" {
+		t.Errorf("Expected Type: string, got: %+v\n", nullColumn)
+	}
+
 	if err != nil {
 		t.Error(err)
 	}
