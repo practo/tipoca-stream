5b57de07dcbe891a98e051fe01301d8cbf5138be
Alok Kumar Singh <alok.singh@practo.com>
2020-09-14 09:51:14 +0000

    Added verbosity

---
+++ kafka-go/build/reset-kafka-offset.sh
@@ -0,0 +1,1 @@
+/bin/kafka-consumer-groups.sh --bootstrap-server kafka-a.practo.com:31466 --group oneness-loader --topic loader-oneness.oneness_db.account_sessions --reset-offsets --to-earliest --execute
