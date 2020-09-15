09354fa334950af920bfc1506c8e0f3bf7935562
Alok Kumar Singh <mail.alok87@gmail.com>
2020-09-15 23:36:13 +0530

    Bug fix for type not being passed and test cases

--- kafka-go/pkg/redshift/redshift_test.go
+++ kafka-go/pkg/redshift/redshift_test.go
@@ -4,68 +4,37 @@ import (
 	"testing"
 )
 
-func TestLongTypeSchema(t *testing.T) {
-	inputTable := Table{
-		Name: "inputTable",
-		Columns: []ColInfo{
-			ColInfo{
-				Name:       "kill_id",
-				Type:       "long",
-				DefaultVal: "",
-				NotNull:    false,
-				PrimaryKey: true,
-			},
-		},
-		Meta: Meta{
-			Schema: "schema",
-		},
-	}
-
-	targetTable := Table{
-		Name: "targetTable",
-		Columns: []ColInfo{
-			ColInfo{
-				Name:       "kill_id",
-				Type:       "character varying(65535)",
-				DefaultVal: "",
-				NotNull:    false,
-				PrimaryKey: true,
-			},
-		},
-		Meta: Meta{
-			Schema: "schema",
-		},
-	}
-
-	columnOps, err := CheckSchemas(inputTable, targetTable)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if len(columnOps) > 0 {
-		t.Error("Schema diff not expected")
-	}
-}
-
 func TestRedshiftDataTypeGet(t *testing.T) {
 	redshiftType, err := GetRedshiftDataType("mysql", "long", "LONGTEXT")
 	if err != nil {
 		t.Error(err)
 	}
-	if redshiftType != "VARCHAR(MAX)" {
-		t.Errorf("Expected redshiftType=VARCHAR(MAX) got=%v\n", redshiftType)
+	if redshiftType != "character varying(max)" {
+		t.Errorf(
+			"Expected redshiftType=character varying(max) got=%v\n",
+			redshiftType)
 	}
 
 	redshiftType, err = GetRedshiftDataType("mysql", "long", "SOMERANDOM")
 	if err != nil {
 		t.Error(err)
 	}
-	if redshiftType != "long" {
-		t.Errorf("Expected redshiftType=long got=%v\n", redshiftType)
+	if redshiftType != "int8" {
+		t.Errorf("Expected redshiftType=int8 got=%v\n", redshiftType)
 	}
 
-	redshiftType, err = GetRedshiftDataType("mongo", "long", "SOMERANDOM")
+	_, err = GetRedshiftDataType("mongo", "long", "SOMERANDOM")
 	if err == nil {
 		t.Error("Expected error to happen for unspported mongo test")
+	}
+
+	redshiftType, err = GetRedshiftDataType("mysql", "string", "MEDIUMTEXT")
+	if err != nil {
+		t.Error(err)
+	}
+	if redshiftType != "character varying(max)" {
+		t.Errorf(
+			"Expected redshiftType=character varying(max) got=%v\n",
+			redshiftType)
 	}
 }
