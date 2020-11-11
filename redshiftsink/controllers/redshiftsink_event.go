e8f16e75a426a28fbb7eae5ce6f9fbc9e87208eb
Alok Kumar Singh <mail.alok87@gmail.com>
2020-11-11 16:58:43 +0530

    Refactored the code

---
+++ redshiftsink/controllers/redshiftsink_event.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"fmt"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+)
+
+const (
+	K8sEventTypeNormal  = "Normal"
+	K8sEventTypeWarning = "Warning"
+)
+
+// ReconcilerEvent represents the action of the operator
+// having actually done anything. Any meaningful change should
+// result in one of these.
+type ReconcilerEvent interface {
+
+	// Record this into an event recorder as a Kubernetes API event
+	Record(recorder record.EventRecorder)
+}
+
+type DeploymentCreatedEvent struct {
+	Object runtime.Object
+	Name   string
+}
+
+func (d DeploymentCreatedEvent) Record(recorder record.EventRecorder) {
+	recorder.Event(d.Object,
+		K8sEventTypeNormal,
+		"DeploymentCreated",
+		fmt.Sprintf("Created deployment with name %q", d.Name))
+}
