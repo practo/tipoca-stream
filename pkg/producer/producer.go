a0ef5fcdbe9c46bb8979e9ce9315956c806e77e7
Alok Kumar Singh <mail.alok87@gmail.com>
2020-12-16 03:31:22 +0530

    Add producer

---
+++ pkg/producer/producer.go
@@ -0,0 +1,136 @@
+package producer
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/Shopify/sarama"
+	"github.com/linkedin/goavro/v2"
+	"github.com/practo/klog/v2"
+	"github.com/practo/tipoca-stream/redshiftsink/pkg/serializer"
+	"github.com/riferrei/srclient"
+	"strings"
+	"time"
+)
+
+type AvroProducer struct {
+	producer sarama.SyncProducer
+	srclient *srclient.SchemaRegistryClient
+}
+
+func NewAvroProducer(brokers []string,
+	kafkaVersion string, schemaRegistryURL string) (*AvroProducer, error) {
+
+	version, err := sarama.ParseKafkaVersion(kafkaVersion)
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing Kafka version: %v\n", err)
+	}
+
+	config := sarama.NewConfig()
+	config.Version = version
+	config.Producer.Partitioner = sarama.NewHashPartitioner
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Return.Successes = true
+	config.Producer.Compression = sarama.CompressionNone
+	config.Producer.MaxMessageBytes = 10000000
+	config.Producer.Retry.Max = 10
+	config.Producer.Retry.Backoff = 1000 * time.Millisecond
+
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AvroProducer{
+		producer: producer,
+		srclient: srclient.CreateSchemaRegistryClient(schemaRegistryURL),
+	}, nil
+}
+
+// CreateSchema creates schema if it does not exist
+func (c *AvroProducer) CreateSchema(
+	topic string, scheme string) (int, bool, error) {
+
+	created := false
+
+	schemeStr := strings.ReplaceAll(scheme, "\n", "")
+	schemeStr = strings.ReplaceAll(schemeStr, " ", "")
+
+	schema, err := serializer.GetLatestSchemaWithRetry(
+		c.srclient, topic, false, 10)
+	if schema == nil || schema.Schema() != schemeStr {
+		klog.V(2).Infof("Creating schema version. topic: %s", topic)
+		schema, err = c.srclient.CreateSchema(
+			topic, scheme, srclient.Avro, false,
+		)
+		if err != nil {
+			return 0, false, err
+		}
+		created = true
+	}
+
+	return schema.ID(), created, nil
+}
+
+func (c *AvroProducer) Add(topic string, schema string,
+	key []byte, value map[string]interface{}) error {
+
+	schemaId, _, err := c.CreateSchema(topic, schema)
+	if err != nil {
+		return err
+	}
+
+	avroCodec, err := goavro.NewCodec(schema)
+	if err != nil {
+		return err
+	}
+
+	binaryValue, err := avroCodec.BinaryFromNative(nil, value)
+	if err != nil {
+		return err
+	}
+
+	binaryMsg := &AvroEncoder{
+		SchemaID: schemaId,
+		Content:  binaryValue,
+	}
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Key:   sarama.StringEncoder(key),
+		Value: binaryMsg,
+	}
+	_, _, err = c.producer.SendMessage(msg)
+	return err
+}
+
+func (c *AvroProducer) Close() {
+	c.producer.Close()
+}
+
+// TODO: move to serializer
+// AvroEncoder encodes schemaId and Avro message.
+type AvroEncoder struct {
+	SchemaID int
+	Content  []byte
+}
+
+func (a *AvroEncoder) Encode() ([]byte, error) {
+	var binaryMsg []byte
+
+	// Serialization format version number; currently always 0.
+	binaryMsg = append(binaryMsg, byte(0))
+
+	// 4-byte schema ID as returned by Schema Registry
+	binarySchemaId := make([]byte, 4)
+	binary.BigEndian.PutUint32(binarySchemaId, uint32(a.SchemaID))
+	binaryMsg = append(binaryMsg, binarySchemaId...)
+
+	// Avro serialized data in Avro's binary encoding
+	binaryMsg = append(binaryMsg, a.Content...)
+	return binaryMsg, nil
+}
+
+// Length of schemaId and Content.
+func (a *AvroEncoder) Length() int {
+	return 5 + len(a.Content)
+}
