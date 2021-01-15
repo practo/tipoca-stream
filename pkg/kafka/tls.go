13b0f8f27c085474cfe545f5bd09afe33582d99f
Alok Kumar Singh <mail.alok87@gmail.com>
2021-01-15 18:43:30 +0530

    Revert "No decoding required"
    This reverts commit d0a3ffe5659cc5f24b278c1480152ec8c4e53fa7.

--- pkg/kafka/tls.go
+++ pkg/kafka/tls.go
@@ -3,10 +3,11 @@ package kafka
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/base64"
 	"fmt"
 )
 
-// TLSConfig stores the string for all the certificate and keys
+// TLSConfig stores the base64 encoded string for all the certificate and keys
 // required for the TLS setup and authentication with kafka for the client
 type TLSConfig struct {
 	Enable   bool   `yaml:"enable"`
@@ -18,10 +19,22 @@ type TLSConfig struct {
 func NewTLSConfig(configTLS TLSConfig) (*tls.Config, error) {
 	tlsConfig := tls.Config{}
 
-	cert, err := tls.X509KeyPair(
-		[]byte(configTLS.UserCert),
-		[]byte(configTLS.UserKey),
-	)
+	userCert, err := base64.StdEncoding.DecodeString(configTLS.UserCert)
+	if err != nil {
+		return &tlsConfig, err
+	}
+
+	userKey, err := base64.StdEncoding.DecodeString(configTLS.UserKey)
+	if err != nil {
+		return &tlsConfig, err
+	}
+
+	caCert, err := base64.StdEncoding.DecodeString(configTLS.CACert)
+	if err != nil {
+		return &tlsConfig, err
+	}
+
+	cert, err := tls.X509KeyPair([]byte(userCert), []byte(userKey))
 	if err != nil {
 		return &tlsConfig, fmt.Errorf(
 			"Error making keyPair from userCert and userKey, err: %v",
@@ -31,7 +44,7 @@ func NewTLSConfig(configTLS TLSConfig) (*tls.Config, error) {
 	tlsConfig.Certificates = []tls.Certificate{cert}
 
 	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM([]byte(configTLS.CACert))
+	caCertPool.AppendCertsFromPEM([]byte(caCert))
 	tlsConfig.RootCAs = caCertPool
 	tlsConfig.BuildNameToCertificate()
 
