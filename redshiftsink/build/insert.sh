b7396a487aca443bf3daa02002daf64b85c9d624
Alok Kumar Singh <alok.singh@practo.com>
2021-01-19 14:07:31 +0000

    Updated inserts script and added manifests dir

--- redshiftsink/build/insert.sh
+++ redshiftsink/build/insert.sh
@@ -3,7 +3,22 @@
 ## Example: ./insert.sh 10
 ## this inserts 10 new rows in inventory.customers table
 
+if [ -z "$1" ]; then
+    echo "kubectl context missing"
+    exit 1
+else
+    context=$1
+fi
+
+if [ -z "$2" ]; then
+    echo "no of records to insert missing"
+    exit 1
+else
+    records=$2
+fi
+
+
 set -e
 
-echo "generating load"
-kubectl get pods -n kafka | grep inventory-mysql  | awk '{print $1}' | xargs -I {} kubectl exec {} -n kafka -- bash /usr/local/bin/load.sh $1
+echo "Inserting"
+kubectl --context=${records} get pods -n kafka | grep inventory-mysql | awk '{print $1}' | xargs -I {} kubectl --context=${records} exec {} -n kafka -- bash /usr/local/bin/load.sh ${records}
