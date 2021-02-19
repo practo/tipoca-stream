c6b33133d18cba125e7fe49c672f6d9cddaf8cee
Alok Kumar Singh <mail.alok87@gmail.com>
2021-02-19 12:58:04 +0530

    Random validity for cache to distribute kakfa requests

--- controllers/sink_group_controller.go
+++ controllers/sink_group_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -594,7 +595,10 @@ func (s *sinkGroup) topicRealtime(
 	cacheLoaded, ok := cache.Load(topic)
 	if ok {
 		realtimeCache = cacheLoaded.(kafkaRealtimeCache)
-		if cacheValid(time.Second*time.Duration(30), realtimeCache.lastCacheRefresh) {
+
+		validitySeconds := rand.Intn(180-30+1) + 2
+		klog.V(2).Infof("rsk/%s validity check: %s s topic: %s", s.rsk.Name, validitySeconds, topic)
+		if cacheValid(time.Second*time.Duration(validitySeconds), realtimeCache.lastCacheRefresh) {
 			klog.V(2).Infof("rsk/%s (realtime cache hit) topic: %s", s.rsk.Name, topic)
 			if realtimeCache.realtime {
 				return true, realtimeCache.lastCacheRefresh, nil
