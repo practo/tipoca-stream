6165dd748659348e3f5d0e7cfde8460a5322cc28
Alok Kumar Singh <mail.alok87@gmail.com>
2021-04-08 10:25:03 +0530

    Fix mask status update

--- redshiftsink/controllers/redshiftsink_controller.go
+++ redshiftsink/controllers/redshiftsink_controller.go
@@ -401,7 +401,8 @@ func (r *RedshiftSinkReconciler) reconcile(
 	// Safety checks
 	if currentMaskVersion == desiredMaskVersion {
 		if len(status.reloading) > 0 && len(status.diffTopics) == 0 {
-			klog.Fatalf("rsk/%s unexpected status, no diff but reloading", rsk.Name)
+			klog.Errorf("rsk/%s unexpected status, no diff but reloading", rsk.Name)
+			status.fixMaskStatus()
 		}
 
 		if len(status.released) == 0 {
